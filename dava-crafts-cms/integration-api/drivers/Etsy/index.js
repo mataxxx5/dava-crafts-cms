8c5fa295e9b61eaae999caa8fd3aa846d5faedf7
mataxxx5 <m.bartosevicius@gmail.com>
2020-07-09 15:25:04 +0100

    added stubs

--- dava-crafts-cms/integration-api/drivers/Etsy/index.js
+++ dava-crafts-cms/integration-api/drivers/Etsy/index.js
@@ -34,6 +34,15 @@ const createListing = async (data) => {
     .catch(e => {console.log("error: ",  e)});
 }
 
+const updateListing = () => {
+    // stub 
+}
+
+const deleteListing = () => {
+    // stub 
+}
+
+
 const buildQueryStr = (data) => {
     // currently static values draft state for testing and only available shipping template
     let queryStr = ETSY_API_URL + "state="+ TESTING_STATE + "&shipping_template_id=100254567443";
@@ -55,5 +64,7 @@ formatData = (data) => {
 
 module.exports = {
     createListing,
+    updateListing,
+    deleteListing,
     formatData
 }
