855f9e31bb9aaea9325abadba8317b967434c9cd
mataxxx5 <m.bartosevicius@gmail.com>
2020-07-14 23:08:45 +0100

    response metadata for new listing creations saved, should be done more robustly late, looks like ass right now

--- lapis/api/etsy-listing/models/etsy-listing.js
+++ lapis/api/etsy-listing/models/etsy-listing.js
@@ -12,8 +12,21 @@ module.exports = {
         },
         async afterCreate(result, data) {
             let results = await integrationAPI.createListing(result);
-            console.log("statusList: ", results[0]);
+            console.log("statusList: ", results[0].entry.results);
             console.log("image object: ", results[0].imageRes[0]);
+            strapi.services['etsy-listing-images'].create(
+                {      
+                    media_references: {
+                        listingId: results[0].entry.results[0].listing_id, 
+                        listingImageData: results[0].imageRes.map(datum => ({
+                            listing_image_id: datum.results[0].listing_image_id,
+                            image_hash: datum.params.image.name
+                        }))
+                    }
+                }
+            );
+
+
             // delete the record or give notification if any other status code than two hundred returns
         } 
     }
